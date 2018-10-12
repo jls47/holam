854135c840bfa249d062ed5e75c96a9e779b2ed8
Schaefer <Lschaefer@hollandamerica.com>
2018-10-12 10:50:49 -0700

    pruned .csv

--- blah.py
+++ blah.py
@@ -3,10 +3,25 @@ import urllib.request
 import pandas as pd
 
 def compare(fname1, fname2):
+	data1 = []
+	data2 = []
 	with open(fname1) as csvfile:
 		reader = pd.read_csv(fname1)
 		data = reader
+		
 		print(data)
+		for thing in data:
+			data1.append(thing)
+			
+	with open(fname2) as csvfile:
+		reader = pd.read_csv(fname1)
+		data = reader
+		
+		print(data)
+		for thing in data:
+			data2.append(thing)
+	print(data1)
+	print(data2)
 
 		
 compare('try1.csv', 'try2.csv')
